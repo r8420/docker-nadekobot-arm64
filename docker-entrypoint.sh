3706b29eeda6c3aba64fa04f948a0ee445fddcbf
Richard Gonlag <rdgonlag@gmail.com>
2025-04-18 19:56:37 +0200

    Enable yt‑dlp auto‑update at container start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-
 set -e
 
 data_init="/app/data_init"
 data="/app/data"
 
 ls $data
-# Merge data_init into data without overwrites.
-# TODO: Barkranger will hopefull fix this, move for testing
-cp -R -n $data_init/* $data
+cp -R -n $data_init/* "$data"
 cp -n "$data_init/creds_example.yml" "$data/creds.yml"
-
 ls $data
 
 echo "Yt-dlp update"
-# TODO: Update yt-dlp. It should not crash the entrypoint if ca-certificates is not installed
-# yt-dlp -U
+command -v yt-dlp >/dev/null && yt-dlp -U || echo "yt-dlp update skipped"
 
 echo "Running NadekoBot"
 exec "$@"
